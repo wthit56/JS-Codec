4b206d56ba3fe37978fc98ba8529b31080b6a185
Thomas Giles <wthit56@googlemail.com>
2016-08-18 11:04:22 +0100

    More checks, can recurse into array items

--- tests/test.similar.array.js
+++ tests/test.similar.array.js
@@ -1,12 +1,18 @@
-module.exports = function similar_array(a, b) {
-	if (a.length !== b.length) {
-		console.log("similar array length failure :: " + a.length + " " + b.length);
+var similar = module.exports = function similar_array(a, b) {
+	// console.log("similar array ::", a, b);
+	
+	if (!a || !b) {
+		return false;
+	}
+	else if (!(a instanceof Array) || !(b instanceof Array)) {
+		return false;
+	}
+	else if (a.length !== b.length) {
 		return false;
 	}
 	else {
 		for (var i = 0, l = a.length; i < l; i++) {
-			if (a[i] !== b[i]) {
-				console.log("similar array failure ::", a, b);
+			if ((a[i] !== b[i]) && !similar(a[i], b[i])) {
 				return false;
 			}
 		}
